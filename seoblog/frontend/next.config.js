086cced54170533b5255e22f14c5c2c6c06e17c7
mounika123chowdary <mounikabellamkonda83@gmail.com>
2021-07-20 22:01:43 +0530

    first commit

---
+++ seoblog/frontend/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    publicRuntimeConfig:{
+        APP_NAME : 'Placement-Blogs',
+        API_DEVELOPMENT: 'http://localhost:8000/api',
+        PRODUCTION: false,
+        DOMAIN_DEVELOPMENT:'https://localhost:3000',
+        DOMAIN_PRODUCTION:'https://blogging.com',
+        DISQUS_SHORTNAME:'blogging-16',
+        GOOGLE_CLIENT_ID:'515722740762-dvp15sjfj31e1ebcecdpvj0ctfg9tiv1.apps.googleusercontent.com'
+    },
+};
---
+++ seoblog/frontend/pages/index.js
@@ -0,0 +1,32 @@
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const Index = () => {
+    return (
+        <Layout>
+            <article className="overflow-hidden">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <h1 className="display-4 font-weight-bold">
+                                Informative Blogs
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 text-center pt-4 pb-5">
+                            <p className="lead">
+                                Campus level
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </Layout>
+    );
+};
+
+export default Index;
